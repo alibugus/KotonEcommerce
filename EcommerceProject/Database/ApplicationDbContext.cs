379d6f5e06c51169d0370ffa36c07f0d5ddb27d7
alibugus <alibugus@hotmail.com>
2024-07-29 17:51:17 +0300

    identity tekrar tanımlandı

--- EcommerceProject/Database/ApplicationDbContext.cs
+++ EcommerceProject/Database/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using EcommerceProject.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EcommerceProject.Database
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser, AppRole, int>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -26,21 +27,18 @@ namespace EcommerceProject.Database
                 .HasOne(p => p.Brand)
                 .WithMany(b => b.Products)
                 .HasForeignKey(p => p.BrandId)
-                .OnDelete(DeleteBehavior.Cascade); // Eğer silme davranışı belirlemek istiyorsanız
+                .OnDelete(DeleteBehavior.Cascade);
 
             // Seed Category
             modelBuilder.Entity<CategoryModel>().HasData(
                 new CategoryModel { Id = 1, Name = "Category1" },
-                new CategoryModel { Id = 2, Name = "Category2" },
-                new CategoryModel { Id = 3, Name = "Category3" },
-                new CategoryModel { Id = 4, Name = "Category4" }
+                new CategoryModel { Id = 2, Name = "Category2" }
             );
 
             // Seed Brand
             modelBuilder.Entity<BrandModel>().HasData(
                 new BrandModel { Id = 1, Name = "Brand1" },
-                new BrandModel { Id = 2, Name = "Brand2" },
-                new BrandModel { Id = 3, Name = "Brand3" }
+                new BrandModel { Id = 2, Name = "Brand2" }
             );
 
             // Seed Product
@@ -70,58 +68,6 @@ namespace EcommerceProject.Database
                     CategoryId = 2,
                     BrandId = 2,
                     ImageUrl = "image2.jpg"
-                },
-                new ProductModel
-                {
-                    Id = 3,
-                    Name = "Product3",
-                    Price = 15.0m,
-                    Description = "Description3",
-                    StockQuantity = 150,
-                    Size = "S",
-                    Color = "Green",
-                    CategoryId = 1,
-                    BrandId = 2,
-                    ImageUrl = "image3.jpg"
-                },
-                new ProductModel
-                {
-                    Id = 4,
-                    Name = "Product4",
-                    Price = 25.0m,
-                    Description = "Description4",
-                    StockQuantity = 250,
-                    Size = "XL",
-                    Color = "Yellow",
-                    CategoryId = 2,
-                    BrandId = 1,
-                    ImageUrl = "image4.jpg"
-                },
-                new ProductModel
-                {
-                    Id = 5,
-                    Name = "Product5",
-                    Price = 30.0m,
-                    Description = "Description5",
-                    StockQuantity = 300,
-                    Size = "M",
-                    Color = "Black",
-                    CategoryId = 3,
-                    BrandId = 1,
-                    ImageUrl = "image5.jpg"
-                },
-                new ProductModel
-                {
-                    Id = 6,
-                    Name = "Product6",
-                    Price = 35.0m,
-                    Description = "Description6",
-                    StockQuantity = 350,
-                    Size = "L",
-                    Color = "White",
-                    CategoryId = 4,
-                    BrandId = 3,
-                    ImageUrl = "image6.jpg"
                 }
             );
         }
